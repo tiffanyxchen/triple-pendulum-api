34bb3aa335fcce53bd5367421733ce8c050e7352
tiffanyxchen <xing.t.chen@gmail.com>
2025-11-17 15:21:51 -0800

    update main allowing for frontend requests

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,16 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'http://localhost:5173',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    },
+  });
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
