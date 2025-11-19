4dfcdef9b6e448cc31015cc39ab15da3b68029f6
tiffanyxchen <xing.t.chen@gmail.com>
2025-11-18 23:34:21 -0800

    update app.module to expose git folder with Nest's static server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 import { UsersModule } from './users/users.module';
 import { OrdersModule } from './orders/orders.module';
 import { ResultsModule } from './results/results.module';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'results'),  // serve TRIPLE-PENDULUM/results/
+      serveRoot: '/results',                       // URL prefix: http://localhost:3000/results/...
+    }),
     UsersModule,   // ← REQUIRED
     OrdersModule,
     ResultsModule,
